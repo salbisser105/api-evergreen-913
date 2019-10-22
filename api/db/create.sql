831ece3d2aea592d429ff429dfb06a41273e1962
salbisser105 <41337754+salbisser105@users.noreply.github.com>
2019-10-22 16:47:58 -0500

    Delete create.sql

--- api/db/create.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE predios (
-    codigo VARCHAR(20) PRIMARY KEY,
-    latitud VARCHAR(200) NOT NULL,
-    longitud VARCHAR(100) NOT NULL,
-    producto VARCHAR(100),
-    area VARCHAR(200),
-    imagen VARCHAR(200)
- )
